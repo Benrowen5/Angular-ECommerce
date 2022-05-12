c5641fcb3be0b156c8f3721dcd3b0d3e709e0591
Benjamin Rowen <Benjamin.Rowen@safeguardproperties.com>
2022-05-12 15:53:34 -0400

    Basic routing set up.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,22 @@ import { Component } from '@angular/core';
   // templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
   template: `
-    <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>
+      {{pageTitle}}
+    </a>  
+    <ul class='nav nav-pills'>
+      <li>
+        <a class='nav-link' routerLink='/welcome'>Home</a>
+      </li>
+      <li>
+        <a class='nav-link' routerLink='/products'>Product List</a>
+      </li>
+    </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
+  </div>
   `
 })
 export class AppComponent {
