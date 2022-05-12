318285c982e557b8521b6138079dd1154bff66cb
Benjamin Rowen <Benjamin.Rowen@safeguardproperties.com>
2022-05-12 14:11:05 -0400

    set up product service and injected into product-list component, added httpClient and Observable for getting products.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component ({ 
     selector: 'pm-products',
@@ -7,11 +9,13 @@ import { IProduct } from './product';
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle = 'Product List';
     imageWidth = 50;
     imageMargin = 2;
     showImage: boolean = false;
+    errorMessage = '';
+    sub!: Subscription;
     
     private _listFilter: string ='';
     get listFilter(): string {
@@ -25,47 +29,9 @@ export class ProductListComponent implements OnInit {
 
     filteredProducts: IProduct[] = [];
 
-    products: IProduct[] = [
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18,2021",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png",
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TDX-0048",
-        "releaseDate": "May 21,2021",
-        "description": "Curved clas steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png",
-    },
-    {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "assets/images/saw.png"
-    },
-    {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2020",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "assets/images/xbox-controller.png"
-    }];
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService) {}
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
@@ -81,6 +47,16 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.listFilter = 'cart';
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            }, 
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 }
